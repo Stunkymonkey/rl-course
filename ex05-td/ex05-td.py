7d8d61b260a870033e16f07a66011919afb4c531
Felix Buehler <account@buehler.rocks>
2020-05-21 10:52:09 +0200

    done 5.2 a&b

--- ex05-td/ex05-td.py
+++ ex05-td/ex05-td.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import gym
 import numpy as np
 from itertools import product
@@ -6,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def print_policy(Q, env):
     """ This is a helper function to print a nice policy from the Q function"""
-    moves = [u'←', u'↓',u'→', u'↑']
+    moves = [u'←', u'↓', u'→', u'↑']
     if not hasattr(env, 'desc'):
         env = env.env
     dims = env.desc.shape
@@ -17,8 +19,8 @@ def print_policy(Q, env):
         policy[idx] = moves[np.argmax(Q[s])]
         if env.desc[idx] in ['H', 'G']:
             policy[idx] = u'·'
-    print('\n'.join([''.join([u'{:2}'.format(item) for item in row]) 
-        for row in policy]))
+    print('\n'.join([''.join([u'{:2}'.format(item) for item in row])
+                     for row in policy]))
 
 
 def plot_V(Q, env):
@@ -33,13 +35,13 @@ def plot_V(Q, env):
         V[idx] = np.max(Q[s])
         if env.desc[idx] in ['H', 'G']:
             V[idx] = 0.
-    plt.imshow(V, origin='upper', 
-               extent=[0,dims[0],0,dims[1]], vmin=.0, vmax=.6, 
+    plt.imshow(V, origin='upper',
+               extent=[0, dims[0], 0, dims[1]], vmin=.0, vmax=.6,
                cmap=plt.cm.RdYlGn, interpolation='none')
     for x, y in product(range(dims[0]), range(dims[1])):
-        plt.text(y+0.5, dims[0]-x-0.5, '{:.3f}'.format(V[x,y]),
-                horizontalalignment='center', 
-                verticalalignment='center')
+        plt.text(y + 0.5, dims[0] - x - 0.5, '{:.3f}'.format(V[x, y]),
+                 horizontalalignment='center',
+                 verticalalignment='center')
     plt.xticks([])
     plt.yticks([])
 
@@ -54,62 +56,84 @@ def plot_Q(Q, env):
         env = env.env
     dims = env.desc.shape
 
-    up = np.array([[0, 1], [0.5, 0.5], [1,1]])
-    down = np.array([[0, 0], [0.5, 0.5], [1,0]])
-    left = np.array([[0, 0], [0.5, 0.5], [0,1]])
-    right = np.array([[1, 0], [0.5, 0.5], [1,1]])
+    up = np.array([[0, 1], [0.5, 0.5], [1, 1]])
+    down = np.array([[0, 0], [0.5, 0.5], [1, 0]])
+    left = np.array([[0, 0], [0.5, 0.5], [0, 1]])
+    right = np.array([[1, 0], [0.5, 0.5], [1, 1]])
     tri = [left, down, right, up]
     pos = [[0.2, 0.5], [0.5, 0.2], [0.8, 0.5], [0.5, 0.8]]
-    
+
     cmap = plt.cm.RdYlGn
-    norm = colors.Normalize(vmin=.0,vmax=.6)
-    
-    ax.imshow(np.zeros(dims), origin='upper', extent=[0,dims[0],0,dims[1]], vmin=.0, vmax=.6, cmap=cmap)
+    norm = colors.Normalize(vmin=.0, vmax=.6)
+
+    ax.imshow(np.zeros(dims), origin='upper', extent=[0, dims[0], 0, dims[1]], vmin=.0, vmax=.6, cmap=cmap)
     ax.grid(which='major', color='black', linestyle='-', linewidth=2)
 
     for s in range(len(Q)):
         idx = np.unravel_index(s, dims)
         x, y = idx
         if env.desc[idx] in ['H', 'G']:
-            ax.add_patch(patches.Rectangle((y, 3-x), 1, 1, color=cmap(.0)))
-            plt.text(y+0.5, dims[0]-x-0.5, '{:.2f}'.format(.0),
-                horizontalalignment='center', 
-                verticalalignment='center')
+            ax.add_patch(patches.Rectangle((y, 3 - x), 1, 1, color=cmap(.0)))
+            plt.text(y + 0.5, dims[0] - x - 0.5, '{:.2f}'.format(.0),
+                     horizontalalignment='center',
+                     verticalalignment='center')
             continue
         for a in range(len(tri)):
-            ax.add_patch(patches.Polygon(tri[a] + np.array([y, 3-x]), color=cmap(Q[s][a])))
-            plt.text(y+pos[a][0], dims[0]-1-x+pos[a][1], '{:.2f}'.format(Q[s][a]), 
+            ax.add_patch(patches.Polygon(tri[a] + np.array([y, 3 - x]), color=cmap(Q[s][a])))
+            plt.text(y + pos[a][0], dims[0] - 1 - x + pos[a][1], '{:.2f}'.format(Q[s][a]),
                      horizontalalignment='center', verticalalignment='center',
-                    fontsize=9, fontweight=('bold' if Q[s][a] == np.max(Q[s]) else 'normal'))
+                     fontsize=9, fontweight=('bold' if Q[s][a] == np.max(Q[s]) else 'normal'))
 
     plt.xticks([])
     plt.yticks([])
 
 
 def sarsa(env, alpha=0.1, gamma=0.9, epsilon=0.1, num_ep=int(1e4)):
-    Q = np.zeros((env.observation_space.n,  env.action_space.n))
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
 
     # TODO: implement the sarsa algorithm
 
     # This is some starting point performing random walks in the environment:
     for i in range(num_ep):
-        s = env.reset()
+        state = env.reset()
         done = False
+        if np.random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :])
         while not done:
-            a = np.random.randint(env.action_space.n)
-            s_, r, done, _ = env.step(a)
+            new_state, reward, done, _ = env.step(action)
+            if np.random.uniform(0, 1) < epsilon:
+                new_action = env.action_space.sample()
+            else:
+                new_action = np.argmax(Q[new_state, :])
+            Q[state, action] = Q[state, action] + alpha * (reward + gamma * Q[new_state, new_action] - Q[state, action])
+            state = new_state
+            action = new_action
     return Q
 
 
 def qlearning(env, alpha=0.1, gamma=0.9, epsilon=0.1, num_ep=int(1e4)):
-    Q = np.zeros((env.observation_space.n,  env.action_space.n))
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
     # TODO: implement the qlearning algorithm
+    for i in range(num_ep):
+        state = env.reset()
+        done = False
+        while not done:
+            if np.random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(Q[state, :])
+            new_state, reward, done, _ = env.step(action)
+            Q[state, action] = Q[state, action] + alpha * \
+                (reward + gamma * np.max(Q[new_state, action] - Q[state, action]))
+            state = new_state
     return Q
 
 
-env=gym.make('FrozenLake-v0')
-#env=gym.make('FrozenLake-v0', is_slippery=False)
-#env=gym.make('FrozenLake-v0', map_name="8x8")
+env = gym.make('FrozenLake-v0')
+# env = gym.make('FrozenLake-v0', is_slippery=False)
+# env = gym.make('FrozenLake-v0', map_name="8x8")
 
 print("Running sarsa...")
 Q = sarsa(env)

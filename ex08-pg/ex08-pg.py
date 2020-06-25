f1f0a6aaadd36954ff932726785bd9c889df025c
Felix Buehler <account@buehler.rocks>
2020-06-25 14:03:53 +0200

    [8.c] done ploting mean

--- ex08-pg/ex08-pg.py
+++ ex08-pg/ex08-pg.py
@@ -37,19 +37,34 @@ def generate_episode(env, theta, display=False):
 def REINFORCE(env):
     theta = np.random.rand(4, 2)  # policy parameters
 
+    ep_lengths = []
+    mean_lengths = {'ep': [], 'length': []}
+
     for e in range(10000):
         if e % 300 == 0:
             states, rewards, actions = generate_episode(env, theta, True)  # display the policy every 300 episodes
         else:
             states, rewards, actions = generate_episode(env, theta, False)
 
-        print("episode: " + str(e) + " length: " + str(len(states)))
-        # TODO: keep track of previous 100 episode lengths and compute mean
+        # keep track of previous 100 episode lengths and compute mean
+        ep_lengths.append(len(states))
+
+        if not e % 100:
+            if e == 0:
+                average_length = ep_lengths[0]
+            else:
+                average_length = sum(ep_lengths[-100:]) / 100
+            mean_lengths['ep'].append(e)
+            mean_lengths['length'].append(average_length)
+            print(f'Episode: {e:>5d}, average length: {average_length:>3.1f}')
 
         # TODO: implement the reinforce algorithm to improve the policy weights
 
-
-
+    plt.plot(mean_lengths['ep'], mean_lengths['length'], label="aggregated min rewards of 100 episodes")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig('length.png')
+    # plt.show()
 
 
 def main():

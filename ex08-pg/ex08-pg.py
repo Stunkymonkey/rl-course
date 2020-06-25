d05c322d3f6094a127fc5a259e5e53e3cf279abe
Felix Buehler <account@buehler.rocks>
2020-06-25 16:31:14 +0200

    [8.c] done

--- ex08-pg/ex08-pg.py
+++ ex08-pg/ex08-pg.py
@@ -3,6 +3,9 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+gamma = 0.95
+alpha = 0.1
+
 
 def policy(state, theta):
     """ return probabilities for actions under softmax action selection """
@@ -10,6 +13,11 @@ def policy(state, theta):
     e = np.exp(scores)
     s = e.sum()
     return e / s
+
+
+def softmax_grad(softmax):
+    s = softmax.reshape(-1, 1)
+    return np.diagflat(s) - np.dot(s, s.T)
 
 
 def generate_episode(env, theta, display=False):
@@ -41,6 +49,8 @@ def REINFORCE(env):
     mean_lengths = {'ep': [], 'length': []}
 
     for e in range(10000):
+        discount = 1
+
         if e % 300 == 0:
             states, rewards, actions = generate_episode(env, theta, True)  # display the policy every 300 episodes
         else:
@@ -59,6 +69,19 @@ def REINFORCE(env):
             print(f'Episode: {e:>5d}, average length: {average_length:>3.1f}')
 
         # TODO: implement the reinforce algorithm to improve the policy weights
+        for t in range(len(states)):
+            G = 0
+            for k in range(t + 1, len(states)):
+                G += np.power(gamma, k - t - 1) * rewards[k]
+
+            state = states[t]
+            action = actions[t]
+            probs = policy(state, theta)
+
+            dsoftmax = softmax_grad(probs)[action]
+            dlog = dsoftmax / probs[action]
+            gradient = state.reshape(1, 4).T.dot(dlog.reshape(1, 2))
+            theta += alpha * G * gradient
 
     plt.plot(mean_lengths['ep'], mean_lengths['length'], label="average episode length of 100 episodes")
     plt.legend(loc=2)
